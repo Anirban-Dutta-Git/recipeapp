c69442631116584274b3496fac0da887b3987675
ANIRBAN DUTTA <anirbandutta011@gmail.com>
2020-12-20 12:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // DB connect
 const client = new Client({
-  user: "$USER",
-  host: "$HOST",
-  database: "$DATABASE",
-  password: "$PASSWORD",
-  port: "$PORT"
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
 });
 
 client.connect();
